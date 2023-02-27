db47ea4c092e47d9af9f740e8e4eef7422eaab9c
Thomas Fournier <thomas42600.f@gmail.com>
2023-02-27 15:14:10 +0100

    Initial Commit

---
+++ FestPlan_MobileApp/FestPlan_MobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestPlan_MobileAppApp.swift
+//  FestPlan_MobileApp
+//
+//  Created by Thomas Fournier on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FestPlan_MobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
