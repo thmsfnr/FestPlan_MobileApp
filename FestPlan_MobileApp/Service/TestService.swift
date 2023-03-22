2005391438d21ac8ab1c8089636007a4e79779ad
Thomas Fournier <thomas42600.f@gmail.com>
2023-03-22 22:17:14 +0100

    add test service

--- FestPlan_MobileApp/Service/TestService.swift
+++ FestPlan_MobileApp/Service/TestService.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+import SwiftUI
+
+class TestService {
+    
+    let apiUrl = ApiConfig().url + "/test/"
+    
+    func isPublic(completion: @escaping (Bool, Error?) -> ()) {
+
+        let url = URL(string: "\(apiUrl)public")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+            
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(false, error)
+                return
+            }
+                
+            guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
+                completion(false, nil)
+                return
+            }
+                
+            completion(true, nil)
+        }.resume()
+    }
+    
+    func isUser(completion: @escaping (Bool, Error?) -> ()) {
+
+        let url = URL(string: "\(apiUrl)user")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("\(AuthHeader.authHeader())", forHTTPHeaderField: "x-access-token")
+            
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(false, error)
+                return
+            }
+                
+            guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
+                completion(false, nil)
+                return
+            }
+                
+            completion(true, nil)
+        }.resume()
+    }
+    
+    func isAdmin(completion: @escaping (Bool, Error?) -> ()) {
+
+        let url = URL(string: "\(apiUrl)admin")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("\(AuthHeader.authHeader())", forHTTPHeaderField: "x-access-token")
+            
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(false, error)
+                return
+            }
+                
+            guard let httpResponse = response as? HTTPURLResponse,(200...299).contains(httpResponse.statusCode) else {
+                completion(false, nil)
+                return
+            }
+                
+            completion(true, nil)
+        }.resume()
+    }
+    
+}
+
