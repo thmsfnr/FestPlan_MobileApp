dafab6e2b4cfc67f56dde786237dc0bae119b82e
Thomas Fournier <thomas42600.f@gmail.com>
2023-03-27 08:51:47 +0200

    add zone management view, zone creation view & zone detail view

--- FestPlan_MobileApp/View/ZoneDetailView.swift
+++ FestPlan_MobileApp/View/ZoneDetailView.swift
@@ -5,4 +5,42 @@
 //  Created by Thomas Fournier on 22/03/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct ZoneDetailView: View {
+    
+    @State var content: ZoneModelView
+    var intent: ZoneListIntent
+    var festival: FestivalModelView
+    @State private var showNewView = false
+    
+    init(content: ZoneModelView, intent: ZoneListIntent, festival: FestivalModelView) {
+        self.content = content
+        self.intent = intent
+        self.festival = festival
+    }
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Nom de la zone", text: $content.nameZone)
+                TextField("Nombre max de bénévoles", value: $content.maxVolunteers, formatter: NumberFormatter())
+                NavigationLink(
+                                destination: ZoneManagementView(model: ZoneListModelView(), festival: festival).navigationBarBackButtonHidden(true),
+                                isActive: $showNewView,
+                                label: {
+                                    Button("Valider") {
+                                        intent.update(idZone: content.idZone, nameZone: content.nameZone, maxVolunteers: content.maxVolunteers)
+                                        showNewView = true
+                                    }
+                                }).navigationBarBackButtonHidden(true)
+            }
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+struct ZoneDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZoneDetailView(content: ZoneModelView(), intent: ZoneListIntent(zoneList: ZoneListModelView()), festival: FestivalModelView())
+    }
+}
--- FestPlan_MobileApp/View/ZoneManagementView.swift
+++ FestPlan_MobileApp/View/ZoneManagementView.swift
@@ -8,16 +8,50 @@
 import SwiftUI
 
 struct ZoneManagementView: View {
+    
+    @ObservedObject var viewModel: ZoneListModelView
+    var festival: FestivalModelView
+    var intent: ZoneListIntent
+    
+    init(model: ZoneListModelView, festival: FestivalModelView) {
+        self.viewModel = model
+        self.intent = ZoneListIntent(zoneList: model)
+        self.festival = festival
+    }
 
     var body: some View {
-        VStack {
-            
+        NavigationView {
+            VStack {
+                NavigationLink(destination: AdminBoardView(model: festival).navigationBarBackButtonHidden(true)) {
+                    Text("Retour")
+                }
+                NavigationLink(destination: ZoneCreationView(content: ZoneModelView(), intent: intent, festival: festival).navigationBarBackButtonHidden(true)) {
+                    Text("Ajouter zone")
+                }
+                List {
+                    ForEach(viewModel.list, id: \.self){item in
+                        NavigationLink(destination: ZoneDetailView(content: item, intent: intent, festival: festival)){
+                            VStack{
+                                Text("\(item.nameZone)")
+                                Text("\(item.maxVolunteers)")
+                            }
+                        }
+                    }
+                    .onDelete { indexSet in
+                        let id = indexSet.map { viewModel.list[$0].idZone }
+                        intent.remove(zone: id[0])
+                    }
+                }
+            }
         }
+        .onAppear(perform: {
+            intent.load(festival: festival.idFestival)
+        })
     }
 }
 
 struct ZoneManagementView_Previews: PreviewProvider {
     static var previews: some View {
-        ZoneManagementView()
+        ZoneManagementView(model: ZoneListModelView(), festival: FestivalModelView())
     }
 }
